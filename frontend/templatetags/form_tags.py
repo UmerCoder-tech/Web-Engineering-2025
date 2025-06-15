c1c66cd7ed19b5b2b043b8aba794005c241a18d5
UmerCoder-tech <mohammed.u.k.hanif@student.fh-kiel.de>
2025-06-15 23:26:25 +0200

    1

--- frontend/templatetags/form_tags.py
+++ frontend/templatetags/form_tags.py
@@ -1,3 +1,4 @@
+
 from django import template
 
 register = template.Library()
@@ -5,3 +6,6 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(field, css_class):
     return field.as_widget(attrs={"class": css_class})
+
+
+
